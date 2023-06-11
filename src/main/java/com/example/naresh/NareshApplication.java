03a41ebc39148f13aaebb7d0a6395d7e8e2548eb
Mavenid <nareshannam96@gmail.com>
2023-06-11 19:06:16 +0100

    initial

---
+++ src/main/java/com/example/naresh/NareshApplication.java
@@ -0,0 +1,13 @@
+package com.example.naresh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NareshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NareshApplication.class, args);
+	}
+
+}
